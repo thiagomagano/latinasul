033c9151927aed583abf3c4a85ba70533ba76ae3
Thiago Magano <thiagomagano1993@outlook.com>
2024-07-09 12:38:59 -0300

    Dummy Pages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["Montserrat", "sans-serif"],
+        body: ["Montserrat", "serif"],
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+
+  // daisyUI config (optional - here are the default values)
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0E3D59",
+          "primary-content": "#FFF",
+          secondary: "#FF6634",
+          "secondary-content": "#FFF",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
+};
