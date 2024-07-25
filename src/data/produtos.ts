b263f69766679c4d4e9de7d5359fe2b7ee22382b
Thiago Magano <thiagomagano1993@outlook.com>
2024-07-25 14:14:15 -0300

    produtos index - placeholders

--- src/data/produtos.ts
+++ src/data/produtos.ts
@@ -1,7 +1,7 @@
 interface produto {
   titulo: string;
   categoria: string;
-  ref: number;
+  ref: string;
   imagem: string;
 }
 
@@ -9,44 +9,110 @@ const listaProduto: produto[] = [
   {
     titulo: "Contra Filé",
     categoria: "Bovinos",
-    ref: 1235,
-    imagem: "carne.png",
+    ref: "0000",
+    imagem: "Bovinos-placeholder.jpg",
+  },
+  // {
+  //   titulo: "Pernil",
+  //   categoria: "Cordeiros",
+  //   ref: "0000",
+  //   imagem: "Cordeiros-placeholder.jpg",
+  // },
+  {
+    titulo: "Costela",
+    categoria: "Suinos",
+    ref: "0000",
+    imagem: "Suinos-placeholder.png",
   },
   {
-    titulo: "Acem",
-    categoria: "Bovinos",
-    ref: 1245,
-    imagem: "placeholder.png",
+    titulo: "Salame",
+    categoria: "Industrializados",
+    ref: "0000",
+    imagem: "Industrializados-placeholder.jpg",
   },
   {
-    titulo: "Franguinho",
+    titulo: "Peito de Frango",
     categoria: "Aves",
-    ref: 256,
-    imagem: "frango.png",
+    ref: "0000",
+    imagem: "Aves-placeholder.jpg",
   },
   {
     titulo: "Pescado",
     categoria: "Peixes",
-    ref: 1125,
+    ref: "0000",
     imagem: "peixe.png",
   },
   {
-    titulo: "Carninha",
-    categoria: "Bovinos",
-    ref: 235,
-    imagem: "carne.png",
+    titulo: "Queijo Minas",
+    categoria: "Queijos",
+    ref: "0000",
+    imagem: "Queijos-placeholder.jpg",
   },
   {
-    titulo: "Contra Filé",
-    categoria: "Bovinos",
-    ref: 15,
-    imagem: "placeholder.png",
+    titulo: "Batata Frita Palito",
+    categoria: "Vegetais",
+    ref: "0000",
+    imagem: "Vegetais-placeholder.jpg",
   },
   {
-    titulo: "Peito",
-    categoria: "Aves",
-    ref: 122,
-    imagem: "frango.png",
+    titulo: "Picanha",
+    categoria: "Bovinos",
+    ref: "0000",
+    imagem: "picanha.jpg",
+  },
+  {
+    titulo: "Costela Janela",
+    categoria: "Bovinos",
+    ref: "0000",
+    imagem: "costela-janela.jpg",
+  },
+  {
+    titulo: "Fralda",
+    categoria: "Bovinos",
+    ref: "0000",
+    imagem: "fralda.jpg",
+  },
+  {
+    titulo: "Pernil",
+    categoria: "Cordeiros",
+    ref: "0000",
+    imagem: "pernil.jpg",
+  },
+  {
+    titulo: "Carré",
+    categoria: "Cordeiros",
+    ref: "0000",
+    imagem: "carre.jpg",
+  },
+  {
+    titulo: "Paleta",
+    categoria: "Cordeiros",
+    ref: "0000",
+    imagem: "paleta.jpg",
+  },
+  {
+    titulo: "Costela",
+    categoria: "Cordeiros",
+    ref: "0000",
+    imagem: "costela.jpg",
+  },
+  {
+    titulo: "Filézinho Suíno",
+    categoria: "Suinos",
+    ref: "0000",
+    imagem: "filezinho-suino.png",
+  },
+  {
+    titulo: "Pernil com Osso e Pele",
+    categoria: "Suinos",
+    ref: "0000",
+    imagem: "pernil-com-osso-e-pele.png",
+  },
+  {
+    titulo: "Sobrepaleta",
+    categoria: "Suinos",
+    ref: "0000",
+    imagem: "sobrepaleta.png",
   },
 ];
 
