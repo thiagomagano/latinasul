fa199df8c9105fd34994ef2ad7a1c788caa40b2e
Thiago Magano <thiagomagano1993@outlook.com>
2024-08-15 12:35:27 -0300

    contato: new visu

--- src/pages/actions/send-email.ts
+++ src/pages/actions/send-email.ts
@@ -10,8 +10,8 @@ export const POST: APIRoute = async ({ request, redirect }) => {
   const email = formData.get("email") as string | null;
   const mensagem = formData.get("mensagem") as string | null;
 
-  const to = "latinasul1@gmail.com";
-  const subject = `Mensagem do site de ${nome}`;
+  const to = "othymag@gmail.com";
+  const subject = `${nome} enviou uma mensagem via site da latina sul`;
 
   // Throw an error if we're missing any of the needed fields.
   if (!nome || !email || !mensagem) {
@@ -21,7 +21,15 @@ export const POST: APIRoute = async ({ request, redirect }) => {
   // Try to send the email using a `sendEmail` function we'll create next. Throw
   // an error if it fails.
   try {
-    const html = `<div>${mensagem} de ${nome} (${email})</div>`;
+    const html = `
+    <div>
+      <h1>${nome} diz:</h1> 
+      <p style="font-size:20px;">${mensagem}</p>
+
+
+      <p style="margin-top:80px;"><b>Contato</b>: ${email}</p>
+    </div>
+    `;
     await sendEmail({ to, subject, html });
   } catch (error) {
     throw new Error("Falha ao enviar email");
